3fad5263827c39adc5bc0024c258f677e2faac63
dinesho <dinesh.opatha@gmail.com>
2016-04-18 10:33:17 +0530

    Update tests file

--- tests/ApilinkTest.php
+++ tests/ApilinkTest.php
@@ -7,7 +7,7 @@ class ApilinkTest extends PHPUnit_Framework_TestCase {
     public function testTimestamp()
     {
         $apiLink = new ApiLink("APIKEY","SECRET",1000);
-        $this->assertTrue($apiLink->TIMESTAMP);
+        $this->assertTrue($apiLink->TIMESTAMP>0);
     }
 
 }
