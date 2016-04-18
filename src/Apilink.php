fd310fcf02979e4b8e37e1b30de8a1b2155ce756
dinesho <dinesh.opatha@gmail.com>
2016-04-18 10:32:38 +0530

    Update Apilink methods into 2 main methods

--- src/Apilink.php
+++ src/Apilink.php
@@ -7,12 +7,15 @@ class Apilink {
     var $API_KEY = null;
     var $SECRET_KEY = null;
     var $SECURITY_TOKEN = null;
-    var $REQUEST_URL = null;
-    var $REQUEST_PARAMETERS = null;
-    var $REQUEST_SIGNATURE = null;
+    var $PAYLOAD = null;
+    var $SIGNATURE = null;
     var $TTL = null;
     var $TIMESTAMP = null;
 
+    /*
+     * Creates and returns an Apilink object for a given
+     * API KEY, SECRET KEY and a TTL
+     */
     public function __construct($api_key, $secret_key, $ttl) {
         $this->API_KEY = $api_key;
         $this->SECRET_KEY = $secret_key;
@@ -22,64 +25,80 @@ class Apilink {
         return $this;
     }
 
+    /*
+     * Creates and Sends a request to the API end point for a given
+     * endpoint url and a payload array.
+     * Returns the result for the request if success
+     */
+    public function sendRequest($url, array $payload){
 
-    public function setRequestURL($url){
-        $this->REQUEST_URL = $url;
-    }
+        // Set payload array and add timestamp and api user
+        // to payload parameters
+        $this->PAYLOAD = $payload;
+        $this->PAYLOAD['signature-timestamp'] = $this->TIMESTAMP;
+        $this->PAYLOAD['signature-user'] = $this->API_KEY;
 
-    public function setPayload(array $parameters){
-        $this->REQUEST_PARAMETERS = $parameters;
-        $this->REQUEST_PARAMETERS['signature-timestamp'] = $this->TIMESTAMP;
-        $this->REQUEST_PARAMETERS['signature-user'] = $this->API_KEY;
-    }
+        // Sign payload for the current request parameters
+        $this->SIGNATURE = hash_hmac('sha256', implode("-",$this->PAYLOAD), $this->SECURITY_TOKEN);
 
-    public function signPayload()
-    {
-        $this->REQUEST_SIGNATURE = hash_hmac('sha256', implode("-",$this->REQUEST_PARAMETERS), $this->SECURITY_TOKEN);
-        $this->REQUEST_PARAMETERS['signature-token'] = $this->REQUEST_SIGNATURE;
-    }
+        // Add signature token to the request parameters
+        $this->PAYLOAD['signature-token'] = $this->SIGNATURE;
 
-    public function sendRequest(){
-
+        // Configure request parameters and get the response
         $options = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
                 'method'  => 'POST',
-                'content' => http_build_query($this->REQUEST_PARAMETERS)
+                'content' => http_build_query($this->PAYLOAD)
             )
         );
         $context  = stream_context_create($options);
-        $result = file_get_contents($this->REQUEST_URL, false, $context);
+        $result = file_get_contents($this->$url, false, $context);
 
         return $result;
     }
 
+    /*
+     * Authenticate a API request when the http request parameter array is passed.
+     * Pass POST/REQUEST array direct into the function after creating a
+     * Apilink object for the request user
+     */
     public function authenticate($httpRequest){
+
+        // Store original timestamp and signature
+        // token came with the request
         $requestTimestamp = $httpRequest['signature-timestamp'];
-        $this->REQUEST_SIGNATURE = $httpRequest['signature-token'];
+        $this->SIGNATURE = $httpRequest['signature-token'];
 
+        // Remove the signature token from request parameters array
         unset($httpRequest['signature-token']);
-        $this->REQUEST_PARAMETERS = $httpRequest;
-        var_dump($this->REQUEST_PARAMETERS);
-        $signatureToken = hash_hmac('sha256', implode("-",$this->REQUEST_PARAMETERS), $this->SECURITY_TOKEN);
 
+        // Set payload to the link and generate signature token
+        $this->PAYLOAD = $httpRequest;
+        $signatureToken = hash_hmac('sha256', implode("-",$this->PAYLOAD), $this->SECURITY_TOKEN);
+
+        // Check the timestamp for expired or not regard to the ttl
         if(($this->TIMESTAMP - $requestTimestamp) < $this->TTL) {
-            if(strcmp($this->REQUEST_SIGNATURE, $signatureToken) == 0){
+            // If request is not expired check the signature validity
+            if(strcmp($this->SIGNATURE, $signatureToken) == 0){
+                // Request signature is valid
                 return array(
                     "CODE" => 200,
                     "INFO" => "Request is valid !"
                 );
             }
             else{
+                // Invalid signature
                 return array(
-                    "CODE" => 500,
+                    "CODE" => 300,
                     "INFO" => "Invalid request signature !"
                 );
             }
         }
         else{
+            // If signature expired already
             return array(
-                "CODE" => 600,
+                "CODE" => 400,
                 "INFO" => "This request has expired !"
             );
         }
